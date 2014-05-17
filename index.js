270a47c8ca6017901477d2a0ec610dd35c45af8d
Trent Lennon <trent.lennon@gmail.com>
2014-05-17 13:23:09 -0400

    Updated the get vms endpoint with the new sql statement Matt came up with. Now joins the tables correctly.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
- 	response.send('Hello World!')
+ 	response.render('index.html');
 })
 
 app.get('/api/test', function(request, response) {
-	response.send("You passed!")
+	response.send("You passed!");
 })
 
 app.listen(app.get('port'), function() {
