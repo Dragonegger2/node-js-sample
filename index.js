44066ba78dfc710fd8db4a14d9d41f9c9520f265
Trent Lennon <trent.lennon@gmail.com>
2014-05-17 15:14:35 -0400

    This is why yo utest locally.

--- index.js
+++ index.js
@@ -56,9 +56,7 @@ app.get('/visits', function(request, response) {
 					console.log(rows);
 
 					res.send(rows);
-				}
-
-});
+				});
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
