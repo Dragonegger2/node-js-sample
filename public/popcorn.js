9c4bf6a1d9a50791f534902ffca9553aa9d41035
Trent Lennon <trent.lennon@gmail.com>
2014-05-18 09:10:48 -0400

    Testing why images are not being drawn. Might be something to do with the way linux does path?

--- public/popcorn.js
+++ public/popcorn.js
@@ -115,7 +115,7 @@ function drawMap(bool){
           position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
           title: parserData[i].get('address'),
           map: map,
-          icon: './markers/green_markerY.png'
+          icon: 'markers/green_markerY.png'
         }); 
     }
     else if (parserData[i].get('reaction') == false ) {
@@ -123,7 +123,7 @@ function drawMap(bool){
           position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
           title: parserData[i].get('address'),
           map: map,
-          icon: './markers/red_markerN.png'
+          icon: 'http://i.imgur.com/d1eamgW.png'
         });
     }
     else {
@@ -131,7 +131,7 @@ function drawMap(bool){
           position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
           title: parserData[i].get('address'),
           map: map,
-          icon: './markers/purple_markerU.png'
+          icon: 'markers/purple_markerU.png'
         });
     }
 
