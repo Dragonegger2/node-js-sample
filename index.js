3333551ce0af76fc8da6198f650fb76ef8b27efe
Trent Lennon <trent.lennon@gmail.com>
2014-07-26 21:51:03 -0400

    Store it

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
 var express = require('express');
 var app = express();
 var mysql = require('mysql');
+var jade = require('jade');
+app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
-
 var connection = mysql.createPool({
-	host:'localhost',
-	user:'root',
-	password:'bitnami',
-	database: "test"
+	host		:	'localhost',
+	user		: 	'root',
+	password	: 	'secret',
+	database 	: 	'lakehost',
+	port: 3306
 });
 
-var PARSE_APPLICATION_ID="hPiQpsKupuMuWRRY0xtnfoY0n3lJnAOoo0QoqiB";
-var REST_API_KEY="hPiQpsKupuMuWRRY0xtnfoY0n3lJnAOoo0QoqiBr";
+app.get('/other', function (req, res) {
+	var data =  {
+            'title': 'Geolocation'
+		};
+  	console.log(data);
+  res.render("index", data);
+});
 
-app.get('/', function(request, response) {
-
- 	response.render('index.html');
-})
-
-app.get('/scout.html', function(request, response) {
-	response.render('/scout.html');
+app.get('/lake', function(request, response) {
+	var getLakeDataSql = "select save.Save_ID, species.Name, lake.Name, lake.Latitude, lake.Longitude, save.Entering from save inner join species ON save.Species_ID = species.species_ID inner join lake ON save.Lake_ID = lake.Lake_ID;";
+	var results;
+	connection.query("use lakehost");
+	connection.query(getLakeDataSql, function(err, rows, fields) {
+			if (err) {
+				response.statusCode = 500;
+				results = {
+					message: "error"
+				}	
+				console.log(err);
+				response.send(results);
+			} else {
+				if(rows.length == 0){
+					rows = { message: "No user found"};
+					response.send(rows);
+					return;
+				} else {
+					console.log(rows);
+					response.send(rows);
+				}
+			}
+		}
+	)
 });
 
 app.get('/test', function(request, response) {
@@ -31,6 +55,7 @@ app.get('/test', function(request, response) {
 		if(err){
 			console.error("Connection error: ", err);
 			response.statusCode = 503;
+			console.log(err);
 			result = {
 				message: "error"
 			}
@@ -40,6 +65,7 @@ app.get('/test', function(request, response) {
 			result = {
 				message: "connected"
 			}
+
 		}
 		response.send(result);
 	}
@@ -64,6 +90,7 @@ app.get('/visits', function(request, response) {
 
 					response.send(rows);
 				}}})});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
--- public/popcorn.js
+++ public/popcorn.js
@@ -1,19 +1,9 @@
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see a blank space instead of the map, this
 // is probably because you have denied permission for location sharing.
-  
-  $(document).ready(function() {
-       $(".cluster").click(function() {
-          var $this = $(this);
-          if($this.is(':checked')) {
-            console.log("checked");
-          }
-          else {
-            console.log("unchecked");
-          }
-          toggleClustering();
-       });
-  });
+
+var latitude = "Latitude";
+var longitude = "Longitude";  
 
   $(document).on('click',".address", function(e) {
     var $this = $(this);
@@ -23,7 +13,7 @@
   }); 
   function changeCenter(lon, lat) {
       mapOptions = {
-          zoom: 15,
+          zoom: 10,
           center: new google.maps.LatLng(lat, lon),
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           maxZoom: 15
@@ -39,50 +29,16 @@
   var map = [];
   var mapOptions = {};
   var mc;
-  var showClustering = false;
+  var showClustering = true;
   var parserData = [];
 
-  function initialize() {
-    if(showClustering){ 
-      $(".cluster").prop('checked', showClustering);
-    }
-     Parse.initialize("3ZZzYfo6sI6gsA12WlOM54E3xKIN3nKuna5gQ7b6", "OiuEdsflpfMoLv1GtsjsXkZIwaigT3LItTNa8ei2");
-      //$("#data").text("parse connection initialized...");
-
-      var Visits = Parse.Object.extend("PopcornVisits");
-      var query = new Parse.Query(Visits);
-      query.find({
-        success: function(results) {
-          parserData = results;
-          var noCount = 0;
-          var yesCount = 0;
-          $("#data").append("<ul>");
-          for(var i = 0; i < results.length; i++) {
-            var reactionClass;
-
-            if(results[i].get("reaction")== true) {
-              reactionClass = "yes";
-              yesCount = yesCount + 1;
-            }
-            else if(results[i].get("reaction") == false) {
-              reactionClass = "no";
-              noCount++;
-            }
-
-            $("#data ul").append("<li class='address " + reactionClass + "' lat=" + results[i].get('latitude') +" lon='"+ results[i].get('longitude') +"'>" + results[i].get("address") + "</li>");
-          } 
-          $("#data").append("</ul>");
-          drawMap(true);
-
-          $("#analytics").append("<table><tr><th>Yes:</th><td>" + yesCount + "</td></tr>" + "<tr><th>No: </th><td>" + noCount + "</td></tr>" + "<tr><th>Average:</th><td>" + yesCount / (yesCount + noCount)+ "</td></tr></table>");
-        },
-        error: function(error) {
-          alert("error");
-        }
-      });
-
-
-  }
+  function init() {
+    $.get( "http://localhost:8080/lake", function( data ) { 
+      //alert(data); myData = data;
+        parserData = data;  
+        drawMap(true);
+    });
+  };
 
 function toggleClustering() {
   if(showClustering) {
@@ -91,11 +47,10 @@ function toggleClustering() {
   else {
     showClustering = true;
   }
-  drawMap(parserData);
+  drawMap(false);
 }
+
 function drawMap(bool){
-  
-  
   latlngbounds = new google.maps.LatLngBounds(); /*AUTOZOOM*/
   
   //Initialize arrays to hold markers and info windows
@@ -103,77 +58,35 @@ function drawMap(bool){
   infowins = [parserData.length]; 
   
   //Set initial map options (start position centered above US)
-  if(bool == true) {
-      mapOptions = {
-          zoom: 6,
-          center: new google.maps.LatLng(parserData[parserData.length-1].get("latitude"), parserData[parserData.length-1].get("longitude")),
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          maxZoom: 15
+  if(bool) {
+    mapOptions = {
+      zoom: 8,        
+      center: new google.maps.LatLng(Number(parserData[parserData.length-1].Latitude), Number(parserData[parserData.length-1].Longitude)),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      maxZoom: 15
+    }
+    for (var i=0;i<parserData.length;i++) {
+            //This is the place where I fill out elements.
+        var temporaryString = parserData[i].Name;
+      $("#data").append("<p class='address' lon='" + Number(parserData[i].Longitude) +"' lat='"+Number(parserData[i].Latitude) + "'>" + temporaryString + "</p>");
     }
   }
-
-  
   //Create map object in the space provided in HTML using set map options
   map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
-  
 
   //Create markers and info windows for each coop in list and store them in previously defined arrays
+  
   for (var i=0;i<parserData.length;i++) {
-    infowins[i] = new google.maps.InfoWindow({
-        content: '<div>' + parserData[i].get("address") + '<br> </div>',       
-        position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
-        maxWidth: 650
-    }); 
-    //By Reaction:
-    if(parserData[i].get('reaction') == true ) {
         markers[i] = new google.maps.Marker({
-          position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
-          title: parserData[i].get('address'),
-          map: map,
-          icon: 'http://i.imgur.com/d1eamgW.png'
+          position: new google.maps.LatLng(Number(parserData[i].Latitude), Number(parserData[i].Longitude)),
+          map: map
         }); 
-    }
-    else if (parserData[i].get('reaction') == false ) {
-        markers[i] = new google.maps.Marker({
-          position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
-          title: parserData[i].get('address'),
-          map: map,
-          icon: 'http://i.imgur.com/OsVByuQ.png'
-        });
-    }
-    else {
-          markers[i] = new google.maps.Marker({
-          position: new google.maps.LatLng(parserData[i].get("latitude"), parserData[i].get("longitude")),
-          title: parserData[i].get('address'),
-          map: map,
-          icon: 'http://i.imgur.com/kcPqXuv.png'
-        });
-    }
-
-    bindInfoWindow(markers[i],map,infowins[i]);
-    //console.log(i + " lon: " + parserData[i].get('longitude') + ' lat: ' + parserData[i].get('latitude'));
   }
+  
   if(showClustering) {
     mc = new MarkerClusterer(map, markers);
     mc.setGridSize(20);
   }
-  /*
-  //Try HTML5 geolocation, if successful store coordinates in pos variable
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-    pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
-    
-    var geo = new google.maps.Geocoder();
-    geo.geocode({'latLng': pos}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK){
-    posadd = results[1].formatted_address;
-    }
-    
-    }); 
-    
-    });
-  }*/
 };
 
 function closeInfoWindow() {
@@ -197,4 +110,4 @@ function bindInfoWindow(marker, map, infowindow){
   google.maps.event.addListener(map, 'click', closeInfoWindow); 
 
   
-window.addEventListener( "load", initialize, false );
+window.addEventListener( "load", init, false );
